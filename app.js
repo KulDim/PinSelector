68db6d7a9a4c5b25f61a9e202d19b5cce00599fb
Вадим <vadim.culya.2017@gmail.com>
2024-09-20 15:16:30 +0300

    express

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const sequelize = require("./contexts/DatabaseContext")
+const router = require("./routes/routes");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use("/api/", router);
+
+(async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        console.log("Connection was established successfully with database");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+    try {
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (error) {
+        console.error(error);
+    }
+})();
