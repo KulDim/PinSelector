2e5cdd692f135704cf278fc981905e2429ed7023
Вадим <vadim.culya.2017@gmail.com>
2024-10-07 15:57:50 +0300

    transfer and renaming

--- routes/apiRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require("express");
-const router = new Router();
-const apiController = require("../controllers/apiController");
-
-router.post("/", apiController.addMicrocontroller);
-// router.get("/", apiController.test);
-
-module.exports = router;
