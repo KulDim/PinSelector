dcae64563f6d7543c7ab5795f99af32da0f1d4da
Вадим <vadim.culya.2017@gmail.com>
2024-10-17 15:42:04 +0300

    SearchMicrocontroller

--- frontend/src/components/SearchMicrocontroller/SearchMicrocontroller.js
+++ frontend/src/components/SearchMicrocontroller/SearchMicrocontroller.js
@@ -1,25 +1,79 @@
-// import React, { useState } from "react";
-import "./SearchMicrocontroller.css";
+import React, { useState } from "react";
+import styles from "./SearchMicrocontroller.module.css";
 
 function SearchMicrocontroller() {
+    const [quantity, setQuantity] = useState("");
+
+    const [pins, setPin] = useState([{ output: "", value: "" }]);
+
+    function addButton() {
+        setPin(pins => [...pins, { output: "", value: "" }]);
+    }
+
+    function dellButton(e) {
+        const id = e.target.getAttribute("data-id");
+        pins.splice(id, 1);
+        setPin(pins => [...pins]);
+    }
+
+    function inputChange(e) {
+        const name = e.target.name;
+        const id = e.target.getAttribute("data-id");
+        pins[id][name] = e.target.value;
+        setPin(pins => [...pins]);
+    }
+
     return (
-        <div className="form">
-            <samp className="form-item">
-                conter <input></input>
-            </samp>
-            <div className="form-item">
-                <span>
-                    pin <input></input>
-                </span>
-                <span>
-                    value <input></input>
-                </span>
+        <div className={styles.form}>
+            <div className={styles.formItem}>
+                <span>Количество выводов:</span>
+                <input
+                    type="number"
+                    value={quantity}
+                    onChange={e => setQuantity(e.target.value)}
+                ></input>
+                <button className={styles.send}>Найти</button>
             </div>
-            <div className="form-item">
-                <button>add</button>
-                <button>dell</button>
+            <div className={styles.overflow}>
+                {pins.map((pin, id) => (
+                    <div
+                        className={styles.formItem}
+                        key={id}
+                    >
+                        <span>Вывод:</span>
+                        <input
+                            type="number"
+                            value={pin.output}
+                            name="output"
+                            onChange={inputChange}
+                            data-id={id}
+                        ></input>
+                        <span>Значение:</span>
+                        <input
+                            value={pin.value}
+                            name="value"
+                            onChange={inputChange}
+                            data-id={id}
+                        ></input>
+                        <button
+                            className={styles.dell}
+                            onClick={dellButton}
+                            data-id={id}
+                        >
+                            Удалить
+                        </button>
+                    </div>
+                ))}
             </div>
-            <button>SearchMicrocontroller</button>
+
+            <div className={styles.formItem}>
+                <button
+                    className={styles.add}
+                    onClick={addButton}
+                >
+                    Добавить
+                </button>
+            </div>
         </div>
     );
 }
