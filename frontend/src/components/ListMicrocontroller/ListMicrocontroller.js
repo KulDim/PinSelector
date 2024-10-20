6e081007fa3466c0750c3f3bf5b2771a4f3a0b43
Вадим <61011733+KulDim@users.noreply.github.com>
2024-10-20 17:22:49 +0300

    ListMicrocontroller

--- frontend/src/components/ListMicrocontroller/ListMicrocontroller.js
+++ frontend/src/components/ListMicrocontroller/ListMicrocontroller.js
@@ -1,5 +1,17 @@
-function ListMicrocontroller() {
-    return <div>ListMicrocontroller</div>;
+import { useState, useEffect } from "react";
+
+function ListMicrocontroller({microcontrollers}) {
+
+    return <div>
+        {microcontrollers.map((microcontroller, index) => (
+            <div key={index}>
+                <span>title:{ microcontroller.title }</span>
+                <span>datasheet:{ microcontroller.datasheet }</span>
+                <span>description:{ microcontroller.description }</span>
+                <span>counter:{ microcontroller.counter }</span>
+            </div>
+        ))}
+    </div>;
 }
 
 export default ListMicrocontroller;
