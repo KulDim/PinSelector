09c4094c19449eb6a11df316a08ca6573e500ceb
Вадим <61011733+KulDim@users.noreply.github.com>
2024-10-20 17:23:05 +0300

    MainPage

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,12 +1,19 @@
 import SearchMicrocontroller from "../components/SearchMicrocontroller/SearchMicrocontroller";
 import ListMicrocontroller from "../components/ListMicrocontroller/ListMicrocontroller";
 import styles from "./MainPage.module.css";
+import { useState } from "react";
 
 function MainPage() {
+    const [microcontrollers, setMicrocontrollers] = useState(null)
+
+    function handleMicrocontrollers(data) {
+        setMicrocontrollers(data.message)
+    }
+
     return (
         <div className={styles.MainPage}>
-            <SearchMicrocontroller />
-            <ListMicrocontroller />
+            <SearchMicrocontroller handleMicrocontrollers={handleMicrocontrollers}/>
+            {microcontrollers && <ListMicrocontroller microcontrollers={microcontrollers}/>}
         </div>
     );
 }
