43576da2fa4d8af0ca1aaaa8c522d3fc726bebb0
Вадим <vadim.culya.2017@gmail.com>
2024-10-24 13:41:45 +0300

    ListMicrocontroller

--- frontend/src/components/ListMicrocontroller/ListMicrocontroller.jsx
+++ frontend/src/components/ListMicrocontroller/ListMicrocontroller.jsx
@@ -1,20 +1,40 @@
 import styles from "./ListMicrocontroller.module.css";
 
-function ListMicrocontroller({microcontrollers, loading}) {
+function ListMicrocontroller({ microcontrollers, loading }) {
+    return (
+        <div>
+            {loading && (
+                <div className={styles.loadingBlock}>
+                    <div className={styles.loading}></div>
+                </div>
+            )}
 
-    return <div className={styles.test}>
-
-        {loading && <div className={styles.loadingBlock}> <div className={styles.loading}></div></div>}
-        
-        {microcontrollers.map((microcontroller, index) => (
-            <div key={index}>
-                <span>title:{ microcontroller.title }</span>
-                <span>datasheet:{ microcontroller.datasheet }</span>
-                <span>description:{ microcontroller.description }</span>
-                <span>counter:{ microcontroller.counter }</span>
-            </div>
-        ))}
-    </div>;
+            {microcontrollers && (
+                <div className={styles.microcontrollers}>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        placeholder="поиск"
+                    />
+                    {microcontrollers.map((microcontroller, index) => (
+                        <div
+                            key={index}
+                            className={styles.microcontroller}
+                        >
+                            <span>
+                                <span>title: {microcontroller.title}</span>
+                                <span>datasheet: {microcontroller.datasheet}</span>
+                            </span>
+                            <span>
+                                <span>description: {microcontroller.description}</span>
+                            </span>
+                            <button>Подробней</button>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
 }
 
 export default ListMicrocontroller;
