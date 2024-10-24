a369377999e120a0dc739643d302972b363bef59
Вадим <vadim.culya.2017@gmail.com>
2024-10-24 13:42:00 +0300

    MainPage

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -4,18 +4,28 @@ import styles from "./MainPage.module.css";
 import { useState } from "react";
 
 function MainPage() {
-    const [microcontrollers, setMicrocontrollers] = useState(null)
-    const [loading, setLoading] = useState(false)
+    const [microcontrollers, setMicrocontrollers] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     function handleMicrocontrollers(data) {
-        setMicrocontrollers(data.message)
-        setLoading(false)
+        if (data === null) {
+            setMicrocontrollers(null);
+            return;
+        }
+        setMicrocontrollers(data.message);
+        setLoading(false);
     }
 
     return (
         <div className={styles.MainPage}>
-            <SearchMicrocontroller handleMicrocontrollers={handleMicrocontrollers} setLoading={setLoading}/>
-            {microcontrollers && <ListMicrocontroller microcontrollers={microcontrollers} loading={loading}/>}
+            <SearchMicrocontroller
+                handleMicrocontrollers={handleMicrocontrollers}
+                setLoading={setLoading}
+            />
+            <ListMicrocontroller
+                microcontrollers={microcontrollers}
+                loading={loading}
+            />
         </div>
     );
 }
